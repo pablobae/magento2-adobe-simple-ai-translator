1a5796afdc1471e5c86b78d3478ccf0e4ccfa18c
Pablo Baenas <pablo.baenas@arcmedia.ch>
2025-03-12 20:46:35 +0100

    Unselect use default when clicking Translate button

--- view/base/web/js/form/element/abstract-mixin.js
+++ view/base/web/js/form/element/abstract-mixin.js
@@ -1,9 +1,10 @@
 define(
     [
         'jquery',
+        'ko',
         'Pablobae_SimpleAiTranslator/js/translator-service'
     ],
-    function ($, TranslatorService) {
+    function ($, ko, TranslatorService) {
         'use strict';
 
         return function (OriginalComponent) {
@@ -17,8 +18,9 @@ define(
                     const storePattern = /\/store\/(\d+)/;
                     const match = pathname.match(storePattern);
 
-                    return match && match[1] ? match[1] : 0; //default store
+                    return match && match[1] ? match[1] : '0'; //default store
                 },
+
                 /**
                  * Call the translate API using the shared utility
                  * @param {Object} data - The data object
@@ -33,6 +35,30 @@ define(
                     // Don't translate if text is empty
                     if (!text || text.trim() === '') {
                         return;
+                    }
+
+                    // If not in default store view, uncheck the "Use Default Value" checkbox
+                    if (storeId !== '0') {
+                        var useDefaultCheckbox = $('input[name="use_default[' + this.index + ']"]');
+                        if (useDefaultCheckbox.length && useDefaultCheckbox.is(':checked')) {
+                            // Find the associated Knockout component
+                            var koContext = ko.contextFor(useDefaultCheckbox[0]);
+                            if (koContext && koContext.$data && koContext.$data.source) {
+                                // Update the Knockout observable directly
+                                var field = this.index;
+                                if (koContext.$data.source.set) {
+                                    koContext.$data.source.set('data.use_default.' + field, false);
+                                }
+                                // Also update the DOM element for good measure
+                                useDefaultCheckbox.prop('checked', false).trigger('change');
+
+                                // Ensure the input is enabled
+                                this.disabled(false);
+                            } else {
+                                // Fallback to just changing the checkbox
+                                useDefaultCheckbox.prop('checked', false).trigger('change');
+                            }
+                        }
                     }
 
                     TranslatorService.translate(text, storeId, function (status, response) {
