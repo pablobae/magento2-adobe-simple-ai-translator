5361f434f6137ffb353d275c2c6f4374b7a03978
Pablo Baenas <pablo.baenas@arcmedia.ch>
2025-03-17 17:24:37 +0100

    Add support to translate CMS pages

--- view/base/web/js/form/element/abstract-mixin.js
+++ view/base/web/js/form/element/abstract-mixin.js
@@ -2,13 +2,27 @@ define(
     [
         'jquery',
         'wysiwygAdapter',
-        'Pablobae_SimpleAiTranslator/js/translator-service'
+        'Pablobae_SimpleAiTranslator/js/translator-service',
+        'mage/translate'
     ],
-    function ($, wysiwygAdapter, TranslatorService) {
+    function ($, wysiwygAdapter, TranslatorService, $t) {
         'use strict';
 
         return function (OriginalComponent) {
             return OriginalComponent.extend({
+                /**
+                 * Check if the current page is a CMS Page or Block edit page
+                 * @returns {boolean}
+                 */
+                isCmsOrBlockEditPage: function () {
+                    const pathname = window.location.pathname;
+
+                    // Define patterns for "Edit CMS Page" and "Edit Block Page"
+                    const editCmsPagePattern = /\/cms\/page\/edit/; // Example: /admin/cms/page/edit/id/1/
+                    const editBlockPagePattern = /\/cms\/block\/edit/; // Example: /admin/cms/block/edit/id/1/
+
+                    return editCmsPagePattern.test(pathname) || editBlockPagePattern.test(pathname);
+                },
                 /**
                  * Retrieve store id from the page URL
                  * @returns {string|null}
@@ -20,7 +34,6 @@ define(
 
                     return match && match[1] ? match[1] : '0'; // Default store
                 },
-
                 /**
                  * Call the translate API and update the field value
                  * @param {Object} data - The data object
@@ -28,18 +41,32 @@ define(
                  */
                 translate: function (data, event) {
                     var self = this;
-                    var text = this.value();
-                    var storeId = this.getStoreIdFromPath();
+                    var text = '';
+                    var storeId = null;
                     var button = event.currentTarget;
+
+                    if (this.isCmsOrBlockEditPage()) {
+                        var storeIdSelect = document.querySelector('[name="store_id"]');
+
+                        if (!storeIdSelect || storeIdSelect.value === '') {
+                            alert($t('Please, select at least one store view from the Page in Websites.'));
+                            return;
+                        }
+                        storeId = storeIdSelect.value;
+                    } else {
+                        storeId = this.getStoreIdFromPath();
+                    }
 
                     // Check if the field is a WYSIWYG editor
                     if (wysiwygAdapter.get(this.uid)) {
-                        text = wysiwygAdapter.get(this.uid).getContent({ format: 'text' });
+                        text = wysiwygAdapter.get(this.uid).getContent({format: 'text'});
+                    } else {
+                        text = this.value();
                     }
 
                     // Don't translate if text is empty
                     if (!text || text.trim() === '') {
-                        alert('No content to translate.');
+                        alert($t('No content to translate.'));
                         return;
                     }
 
@@ -51,12 +78,11 @@ define(
                         $(button).removeClass('loading');
 
                         if (status === 'success') {
-                            // Update WYSIWYG editor or regular field content
+                            // Update WYSIWYG editor
                             if (wysiwygAdapter.get(self.wysiwygId)) {
                                 var wysiwyg = wysiwygAdapter.get(self.wysiwygId).setContent(response);
                             }
                             self.value(response);
-
                         } else {
                             alert('ERROR: ' + response);
                         }
